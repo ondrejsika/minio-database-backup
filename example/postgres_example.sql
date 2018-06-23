b57cc39b1814b8dab19137a45c104b57ac0a6725
Ondrej Sika <ondrej@ondrejsika.com>
2018-06-23 16:56:07 +0200

    add example in Docker

---
+++ example/postgres_example.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE example;
+\connect example
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.3 (Debian 10.3-1.pgdg90+1)
+-- Dumped by pg_dump version 10.3 (Debian 10.3-1.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: hello; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.hello (
+    message character varying(100)
+);
+
+
+ALTER TABLE public.hello OWNER TO postgres;
+
+--
+-- Data for Name: hello; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.hello (message) FROM stdin;
+Hello world
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
